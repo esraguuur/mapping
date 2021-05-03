6f94fdc849df57b2970dfafc1bcc2fc17e373954
esraguuur <esraguuur@gmail.com>
2021-05-03 09:27:51 +0300

    Turkiye haritası,

---
+++ cities.js
@@ -0,0 +1,489 @@
+{
+   "cities": [{
+         "iladi": "ADANA",
+         "enlem": 37,
+         "boylam": 35.321333,
+         "sayi": 1
+      },
+      {
+         "iladi": "ADIYAMAN",
+         "enlem": 37.764751,
+         "boylam": 38.278561,
+         "sayi": 2
+      },
+      {
+         "iladi": "AFYONKARAHİSAR",
+         "enlem": 38.750714,
+         "boylam": 30.556692,
+         "sayi": 3
+      },
+      {
+         "iladi": "AĞRI",
+         "enlem": 39.626922,
+         "boylam": 43.021596,
+         "sayi": 4
+      },
+      {
+         "iladi": "AKSARAY",
+         "enlem": 38.36869,
+         "boylam": 34.03698,
+         "sayi": 5
+      },
+      {
+         "iladi": "AMASYA",
+         "enlem": 40.64991,
+         "boylam": 35.83532,
+         "sayi": 6
+      },
+      {
+         "iladi": "ANKARA",
+         "enlem": 39.92077,
+         "boylam": 32.85411,
+         "sayi": 7
+      },
+      {
+         "iladi": "ANTALYA",
+         "enlem": 36.88414,
+         "boylam": 30.70563,
+         "sayi": 8
+      },
+      {
+         "iladi": "ARDAHAN",
+         "enlem": 41.110481,
+         "boylam": 42.702171,
+         "sayi": 9
+      },
+      {
+         "iladi": "ARTVİN",
+         "enlem": 41.18277,
+         "boylam": 41.818292,
+         "sayi": 10
+      },
+      {
+         "iladi": "AYDIN",
+         "enlem": 37.856041,
+         "boylam": 27.841631,
+         "sayi": 11
+      },
+      {
+         "iladi": "BALIKESİR",
+         "enlem": 39.648369,
+         "boylam": 27.88261,
+         "sayi": 12
+      },
+      {
+         "iladi": "BARTIN",
+         "enlem": 41.581051,
+         "boylam": 32.460979,
+         "sayi": 13
+      },
+      {
+         "iladi": "BATMAN",
+         "enlem": 37.881168,
+         "boylam": 41.13509,
+         "sayi": 14
+      },
+      {
+         "iladi": "BAYBURT",
+         "enlem": 40.255169,
+         "boylam": 40.22488,
+         "sayi": 15
+      },
+      {
+         "iladi": "BİLECİK",
+         "enlem": 40.056656,
+         "boylam": 30.066524,
+         "sayi": 16
+      },
+      {
+         "iladi": "BİNGÖL",
+         "enlem": 39.062635,
+         "boylam": 40.76961,
+         "sayi": 17
+      },
+      {
+         "iladi": "BİTLİS",
+         "enlem": 38.393799,
+         "boylam": 42.12318,
+         "sayi": 18
+      },
+      {
+         "iladi": "BOLU",
+         "enlem": 40.575977,
+         "boylam": 31.578809,
+         "sayi": 19
+      },
+      {
+         "iladi": "BURDUR",
+         "enlem": 37.461267,
+         "boylam": 30.066524,
+         "sayi": 20
+      },
+      {
+         "iladi": "BURSA",
+         "enlem": 40.266864,
+         "boylam": 29.063448,
+         "sayi": 21
+      },
+      {
+         "iladi": "ÇANAKKALE",
+         "enlem": 40.155312,
+         "boylam": 26.41416,
+         "sayi": 22
+      },
+      {
+         "iladi": "ÇANKIRI",
+         "enlem": 40.601343,
+         "boylam": 33.613421,
+         "sayi": 23
+      },
+      {
+         "iladi": "ÇORUM",
+         "enlem": 40.550556,
+         "boylam": 34.955556,
+         "sayi": 24
+      },
+      {
+         "iladi": "DENİZLİ",
+         "enlem": 37.77652,
+         "boylam": 29.08639,
+         "sayi": 25
+      },
+      {
+         "iladi": "DİYARBAKIR",
+         "enlem": 37.91441,
+         "boylam": 40.230629,
+         "sayi": 26
+      },
+      {
+         "iladi": "DÜZCE",
+         "enlem": 40.843849,
+         "boylam": 31.15654,
+         "sayi": 27
+      },
+      {
+         "iladi": "EDİRNE",
+         "enlem": 41.681808,
+         "boylam": 26.562269,
+         "sayi": 28
+      },
+      {
+         "iladi": "ELAZIĞ",
+         "enlem": 38.680969,
+         "boylam": 39.226398,
+         "sayi": 29
+      },
+      {
+         "iladi": "ERZİNCAN",
+         "enlem": 39.75,
+         "boylam": 39.5,
+         "sayi": 30
+      },
+      {
+         "iladi": "ERZURUM",
+         "enlem": 39.9,
+         "boylam": 41.27,
+         "sayi": 31
+      },
+      {
+         "iladi": "ESKİŞEHİR",
+         "enlem": 39.776667,
+         "boylam": 30.520556,
+         "sayi": 32
+      },
+      {
+         "iladi": "GAZİANTEP",
+         "enlem": 37.06622,
+         "boylam": 37.38332,
+         "sayi": 33
+      },
+      {
+         "iladi": "GİRESUN",
+         "enlem": 40.912811,
+         "boylam": 38.38953,
+         "sayi": 34
+      },
+      {
+         "iladi": "GÜMÜŞHANE",
+         "enlem": 40.438588,
+         "boylam": 39.508556,
+         "sayi": 35
+      },
+      {
+         "iladi": "HAKKARİ",
+         "enlem": 37.583333,
+         "boylam": 43.733333,
+         "sayi": 36
+      },
+      {
+         "iladi": "HATAY",
+         "enlem": 36.401849,
+         "boylam": 36.34981,
+         "sayi": 37
+      },
+      {
+         "iladi": "IĞDIR",
+         "enlem": 39.887984,
+         "boylam": 44.004836,
+         "sayi": 38
+      },
+      {
+         "iladi": "ISPARTA",
+         "enlem": 37.764771,
+         "boylam": 30.556561,
+         "sayi": 39
+      },
+      {
+         "iladi": "İSTANBUL",
+         "enlem": 41.00527,
+         "boylam": 28.97696,
+         "sayi": 40
+      },
+      {
+         "iladi": "İZMİR",
+         "enlem": 38.41885,
+         "boylam": 27.12872,
+         "sayi": 41
+      },
+      {
+         "iladi": "KAHRAMANMARAŞ",
+         "enlem": 37.585831,
+         "boylam": 36.937149,
+         "sayi": 42
+      },
+      {
+         "iladi": "KARABÜK",
+         "enlem": 41.2061,
+         "boylam": 32.62035,
+         "sayi": 43
+      },
+      {
+         "iladi": "KARAMAN",
+         "enlem": 37.17593,
+         "boylam": 33.228748,
+         "sayi": 44
+      },
+      {
+         "iladi": "KARS",
+         "enlem": 40.616667,
+         "boylam": 43.1,
+         "sayi": 45
+      },
+      {
+         "iladi": "KASTAMONU",
+         "enlem": 41.38871,
+         "boylam": 33.78273,
+         "sayi": 46
+      },
+      {
+         "iladi": "KAYSERİ",
+         "enlem": 38.73122,
+         "boylam": 35.478729,
+         "sayi": 47
+      },
+      {
+         "iladi": "KIRIKKALE",
+         "enlem": 39.846821,
+         "boylam": 33.515251,
+         "sayi": 48
+      },
+      {
+         "iladi": "KIRKLARELİ",
+         "enlem": 41.733333,
+         "boylam": 27.216667,
+         "sayi": 49
+      },
+      {
+         "iladi": "KIRŞEHİR",
+         "enlem": 39.14249,
+         "boylam": 34.17091,
+         "sayi": 50
+      },
+      {
+         "iladi": "KİLİS",
+         "enlem": 36.718399,
+         "boylam": 37.12122,
+         "sayi": 51
+      },
+      {
+         "iladi": "KOCAELİ",
+         "enlem": 40.85327,
+         "boylam": 29.88152,
+         "sayi": 52
+      },
+      {
+         "iladi": "KONYA",
+         "enlem": 37.866667,
+         "boylam": 32.483333,
+         "sayi": 53
+      },
+      {
+         "iladi": "KÜTAHYA",
+         "enlem": 39.416667,
+         "boylam": 29.983333,
+         "sayi": 54
+      },
+      {
+         "iladi": "MALATYA",
+         "enlem": 38.35519,
+         "boylam": 38.30946,
+         "sayi": 55
+      },
+      {
+         "iladi": "MANİSA",
+         "enlem": 38.619099,
+         "boylam": 27.428921,
+         "sayi": 56
+      },
+      {
+         "iladi": "MARDİN",
+         "enlem": 37.321163,
+         "boylam": 40.724477,
+         "sayi": 57
+      },
+      {
+         "iladi": "MERSİN",
+         "enlem": 36.8,
+         "boylam": 34.633333,
+         "sayi": 58
+      },
+      {
+         "iladi": "MUĞLA",
+         "enlem": 37.215278,
+         "boylam": 28.363611,
+         "sayi": 59
+      },
+      {
+         "iladi": "MUŞ",
+         "enlem": 38.946189,
+         "boylam": 41.753893,
+         "sayi": 60
+      },
+      {
+         "iladi": "NEVŞEHİR",
+         "enlem": 38.69394,
+         "boylam": 34.685651,
+         "sayi": 61
+      },
+      {
+         "iladi": "NİĞDE",
+         "enlem": 37.966667,
+         "boylam": 34.683333,
+         "sayi": 62
+      },
+      {
+         "iladi": "ORDU",
+         "enlem": 40.983879,
+         "boylam": 37.876411,
+         "sayi": 63
+      },
+      {
+         "iladi": "OSMANİYE",
+         "enlem": 37.213026,
+         "boylam": 36.176261,
+         "sayi": 64
+      },
+      {
+         "iladi": "RİZE",
+         "enlem": 41.02005,
+         "boylam": 40.523449,
+         "sayi": 65
+      },
+      {
+         "iladi": "SAKARYA",
+         "enlem": 40.693997,
+         "boylam": 30.435763,
+         "sayi": 66
+      },
+      {
+         "iladi": "SAMSUN",
+         "enlem": 41.292782,
+         "boylam": 36.33128,
+         "sayi": 67
+      },
+      {
+         "iladi": "SİİRT",
+         "enlem": 37.933333,
+         "boylam": 41.95,
+         "sayi": 68
+      },
+      {
+         "iladi": "SİNOP",
+         "enlem": 42.02314,
+         "boylam": 35.153069,
+         "sayi": 69
+      },
+      {
+         "iladi": "SİVAS",
+         "enlem": 39.747662,
+         "boylam": 37.017879,
+         "sayi": 70
+      },
+      {
+         "iladi": "ŞANLIURFA",
+         "enlem": 37.159149,
+         "boylam": 38.796909,
+         "sayi": 71
+      },
+      {
+         "iladi": "ŞIRNAK",
+         "enlem": 37.418748,
+         "boylam": 42.491834,
+         "sayi": 72
+      },
+      {
+         "iladi": "TEKİRDAĞ",
+         "enlem": 40.983333,
+         "boylam": 27.516667,
+         "sayi": 73
+      },
+      {
+         "iladi": "TOKAT",
+         "enlem": 40.316667,
+         "boylam": 36.55,
+         "sayi": 74
+      },
+      {
+         "iladi": "TRABZON",
+         "enlem": 41.00145,
+         "boylam": 39.7178,
+         "sayi": 75
+      },
+      {
+         "iladi": "TUNCELİ",
+         "enlem": 39.307355,
+         "boylam": 39.438778,
+         "sayi": 76
+      },
+      {
+         "iladi": "UŞAK",
+         "enlem": 38.682301,
+         "boylam": 29.40819,
+         "sayi": 77
+      },
+      {
+         "iladi": "VAN",
+         "enlem": 38.48914,
+         "boylam": 43.40889,
+         "sayi": 78
+      },
+      {
+         "iladi": "YALOVA",
+         "enlem": 40.65,
+         "boylam": 29.266667,
+         "sayi": 79
+      },
+      {
+         "iladi": "YOZGAT",
+         "enlem": 39.818081,
+         "boylam": 34.81469,
+         "sayi": 80
+      },
+      {
+         "iladi": "ZONGULDAK",
+         "enlem": 41.456409,
+         "boylam": 31.798731,
+         "sayi": 81
+      }
+   ]
+}
